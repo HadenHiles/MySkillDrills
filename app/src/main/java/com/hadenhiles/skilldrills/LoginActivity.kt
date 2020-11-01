10c6099388f7fbec400b07441b35743662892965
Haden <hadenhiles@gmail.com>
2020-10-31 22:39:27 -0400

    Add login activity for app sign in with email, google, and facebook. Facebook.
    Facebook sign in isn't working due to insufficient permissions when calling the graph api, I believe this to be due to a change in the facebook graph requirements. Need to figure out why

---
+++ app/src/main/java/com/hadenhiles/skilldrills/LoginActivity.kt
@@ -0,0 +1,59 @@
+package com.hadenhiles.skilldrills
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123 // woah ohh oah it's magic
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Initialize the firebase ui intent
+        // Email, Google, Facebook
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    // After the sign in flow is complete, act according to the activity response
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+
+                // Send them to the main activity
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Snackbar.make(this.findViewById(R.id.content), "Invalid Login", Snackbar.LENGTH_SHORT)
+            }
+        }
+    }
+}
