af9ba355b2f5b9318a85028e412126b20b80d14f
Haden <hadenhiles@gmail.com>
2020-11-01 12:26:21 -0500

    Implement custom login layout

--- app/src/main/java/com/hadenhiles/skilldrills/LoginActivity.kt
+++ app/src/main/java/com/hadenhiles/skilldrills/LoginActivity.kt
@@ -2,13 +2,19 @@ package com.hadenhiles.skilldrills
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.ui.idp.AuthMethodPickerActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.custom_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,6 +33,15 @@ class LoginActivity : AppCompatActivity() {
             //  Could be due to a change in the facebook graph api requirements that causes insufficient permissions, disabling for now.
         )
 
+        // Override the firebase ui login layout with my custom layout.
+        // Ensure that the button ID is set for every provider that's enabled.
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.custom_login)
+            .setGoogleButtonId(R.id.googleSignInButton)
+            .setEmailButtonId(R.id.emailSignInButton)
+            //.setFacebookButtonId(R.id.facebookSignInButton)
+            //.setTosAndPrivacyPolicyId(R.id.baz)
+            .build()
+
         // Create and launch sign-in intent
         startActivityForResult(
             AuthUI.getInstance()
@@ -34,6 +49,7 @@ class LoginActivity : AppCompatActivity() {
                 .setTheme(R.style.LoginTheme)
                 .setLogo(R.drawable.skilldrills_light_bg)
                 .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                .setAuthMethodPickerLayout(customLayout) // custom_login layout
                 .setAvailableProviders(providers)
                 .build(),
             RC_SIGN_IN
