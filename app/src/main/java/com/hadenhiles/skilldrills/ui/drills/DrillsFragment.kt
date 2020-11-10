815ce62cc943e047f3e4c36da03e77696b20c560
Haden <hadenhiles@gmail.com>
2020-11-10 10:48:54 -0500

    Start implementing ability to add drills

--- app/src/main/java/com/hadenhiles/skilldrills/ui/drills/DrillsFragment.kt
+++ app/src/main/java/com/hadenhiles/skilldrills/ui/drills/DrillsFragment.kt
@@ -1,32 +1,31 @@
 package com.hadenhiles.skilldrills.ui.drills
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
+import com.hadenhiles.skilldrills.AddDrillActivity
 import com.hadenhiles.skilldrills.R
+import kotlinx.android.synthetic.main.fragment_drills.*
 
 class DrillsFragment : Fragment() {
-
-    private lateinit var drillsViewModel: DrillsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        drillsViewModel = ViewModelProviders.of(this).get(DrillsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_drills, container, false)
-        val textView: TextView = root.findViewById(R.id.text_drills)
+        return inflater.inflate(R.layout.fragment_drills, container, false)
+    }
 
-        drillsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        return root
+        addDrillButton.setOnClickListener {
+            val intent = Intent(activity, AddDrillActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
